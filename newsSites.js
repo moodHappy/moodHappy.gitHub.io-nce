6c05263af00d44013665915e17c95f24e33b71d2
moodHappy <lhwuhuei@gmail.com>
2024-03-03 20:52:43 +0800

    Update newsSites.js

--- newsSites.js
+++ newsSites.js
@@ -1,3 +1,4 @@
+    <script>
 const newsSites = [
     "www.axios.com",
     "https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B0%A1%E9%AB%94/",
@@ -5,3 +6,4 @@ const newsSites = [
 ];
 
 export default newsSites;
+    </script>
