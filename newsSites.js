39dbe9a0cac1e6fb646e403963847c88d54a68e0
moodHappy <lhwuhuei@gmail.com>
2024-03-03 20:44:40 +0800

    Update

---
+++ newsSites.js
@@ -0,0 +1,7 @@
+const newsSites = [
+    "www.axios.com",
+    "https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B0%A1%E9%AB%94/",
+    "https://www.vocabulary.com/"
+];
+
+export default newsSites;
