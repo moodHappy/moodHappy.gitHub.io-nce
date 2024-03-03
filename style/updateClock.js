a2064abde5a26f8912100a388d0334c418f7727c
moodHappy <lhwuhuei@gmail.com>
2024-03-03 22:36:12 +0800

    Update updateClock.js

--- style/updateClock.js
+++ style/updateClock.js
@@ -1,26 +1,35 @@
 function updateClock() {
-var now = new Date();
-var hour = now.getHours();
-var minute = now.getMinutes();
-var second = now.getSeconds();
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
 
-if (hour < 10) {
-hour = "0" + hour;
-}
-if (minute < 10) {
-minute = "0" + minute;
-}
-if (second < 10) {
-second = "0" + second;
-}
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
 
-var time = hour + ":" + minute + ":" + second;
-var clock = document.getElementById("clock");
-clock.textContent = time;
+    var time = hour + ":" + minute + ":" + second;
+    var clock = document.getElementById("clock");
+    clock.textContent = time;
 }
 
 window.onload = updateClock;
 window.setInterval(updateClock, 1000);
+function addEffects() {
+var elements = document.body.getElementsByTagName("*");
+for (var i = 0; i < elements.length; i++) {
+var color = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+elements[i].style.color = color;
+}
+}
+
+window.addEventListener("load", addEffects);
 
 function addEffects() {
 var elements = document.body.getElementsByTagName("*");
